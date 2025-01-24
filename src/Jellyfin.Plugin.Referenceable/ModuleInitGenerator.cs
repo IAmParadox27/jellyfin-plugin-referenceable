0557c6aa2306bc9a2eb9b68862b2d9df56f3fc14
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-01-24 21:07:59 +0000

    Initial commit

---
+++ src/Jellyfin.Plugin.Referenceable/ModuleInitGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Jellyfin.Plugin.Referenceable
+{
+    [Generator]
+    public class ModuleInitGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            Console.WriteLine("Initializing ModuleInitGenerator");
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            using Stream? resourceStreamModuleInitializer = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(ModuleInitGenerator).Namespace}.GeneratorTemplates.ModuleInitializer.cs");
+            using Stream? resourceStreamServiceRegistrator = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{typeof(ModuleInitGenerator).Namespace}.GeneratorTemplates.ServiceRegistrator.cs");
+
+            if (resourceStreamModuleInitializer != null)
+            {
+                using StreamReader reader = new StreamReader(resourceStreamModuleInitializer, Encoding.UTF8);
+            
+                context.AddSource("ModuleInitializer.g.cs", SourceText.From(reader.ReadToEnd()
+                    .Replace("{{namespace}}", context.Compilation.Assembly.Name), Encoding.UTF8));
+            }
+
+            if (resourceStreamServiceRegistrator != null)
+            {
+                using StreamReader reader = new StreamReader(resourceStreamServiceRegistrator, Encoding.UTF8);
+            
+                context.AddSource("ServiceRegistrator.g.cs", SourceText.From(reader.ReadToEnd()
+                    .Replace("{{namespace}}", context.Compilation.Assembly.Name), Encoding.UTF8));
+            }
+        }
+    }
+}
---
+++ src/Jellyfin.Plugin.Referenceable/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: SuppressMessage("Intentional", "CA2255", Justification = "In order for Jellyfin plugins to be referenceable by other plugins they must be initialized in a non-collectable AssemblyLoadContext. This dll is designed to be a starting point for those types of plugins to avoid individual plugin devs from needing to do this.")]
