f30b6e52a2971fcb7f0a89718ce958658c7f473c
IAmParadox27 <zach.ross-clyne@hotmail.co.uk>
2025-01-25 01:25:39 +0000

    Bumped to 1.0.1 cont.

--- src/Jellyfin.Plugin.Referenceable/Properties/AssemblyInfo.cs
+++ src/Jellyfin.Plugin.Referenceable/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 [assembly: SuppressMessage("Intentional", "CA2255", Justification = "In order for Jellyfin plugins to be referenceable by other plugins they must be initialized in a non-collectable AssemblyLoadContext. This dll is designed to be a starting point for those types of plugins to avoid individual plugin devs from needing to do this.")]
